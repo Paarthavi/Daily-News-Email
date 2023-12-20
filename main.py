7778471152ec1cefa46663f37433f638bb5619f4
Paarthavi <paarthavih2@gmail.com>
2023-12-20 14:00:24 +0530

    Day 28: Add links, specify language, limit news, add subject and control category

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import os
 from send_email import send_email
 from email.mime.text import MIMEText
 
+topic = "tesla"
 api_key = os.getenv("API_KEY")
-url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=a285188888e74daca5d5578b01a3aac2"
+url = f"https://newsapi.org/v2/everything?" \
+	  f"q={topic}&" \
+	  "sortBy=publishedAt&" \
+	f"apiKey={api_key}&" \
+	"language=en"
 # &from=2023-11-19
 
 # Make a request
@@ -14,16 +19,17 @@ request = requests.get(url)
 content = request.json()
 
 # Access the articles titles and descriptions
-body = ""
-for article in content["articles"]:
+body = "Subject: Today's news\n"
+for article in content["articles"][:20]:
 	#print(article["title"])
 	#print(article["description"])
 	title = article["title"]
 	description = article["description"]
+	article_url = article["url"]
 
 	# Check if description is not None before concatenating
 	if description is not None:
-		body += title + "\n" + description + 2*"\n"
+		body = body + title + "\n" + description + "\n" + article_url + 2*"\n"
 
 		format_text = MIMEText(body, 'plain', 'utf-8')
 
